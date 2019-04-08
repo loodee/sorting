31ab3682f17791369d648b7f367f8dfef4856ed8
Olof Stacke <stacke@student.chalmers.se>
2019-04-08 18:01:30 +0200

    Det finns nu fler tester, och dom funkar

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,7 +2,21 @@ import Test.QuickCheck
 import Sorting
 import Data.List
 
-main = quickCheck prop_quick_length
+main =
+    do
+     quickCheckWithResult stdArgs prop_quick_length >>= crash
+     quickCheckWithResult stdArgs prop_quick_elem >>= crash
+     quickCheckWithResult stdArgs prop_quick_head >>= crash
+
+crash :: Result -> IO()
+crash Success {} = return ()
+crash _ = error "Failed test"
 
 prop_quick_length :: [Int] -> Bool
 prop_quick_length xs = (length xs) == length (quicksort xs)
+
+prop_quick_elem :: [Int] -> Bool
+prop_quick_elem (x:xs) = elem x (quicksort (x:xs))
+
+prop_quick_head :: [Int] -> Bool
+prop_quick_head xs = 0 == head( quicksort (0:xs) )
