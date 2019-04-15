50d641f60974dd6420c2b79baa65d66b3dc635f4
Thomas Li <guslithd@student.gu.se>
2019-04-15 09:20:13 +0200

    more properties, including mergesort

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,15 +2,25 @@ import Test.QuickCheck
 import Sorting
 import Data.List
 
-main =
-    do
-     quickCheckWithResult stdArgs prop_quick_length >>= crash
-     quickCheckWithResult stdArgs prop_quick_elem >>= crash
+main = do
+    -- quicksort properties
+    quickCheckWithResult stdArgs prop_quick_sorted >>= crash
+    quickCheckWithResult stdArgs prop_quick_length >>= crash
+    quickCheckWithResult stdArgs prop_quick_elem >>= crash
 
-crash :: Result -> IO()
+    -- merge sort properties
+    quickCheckWithResult stdArgs prop_merge_sorted >>= crash
+    quickCheckWithResult stdArgs prop_merge_length >>= crash
+
+crash :: Result -> IO ()
 crash Success {} = return ()
 crash _ = error "Failed test"
 
+-- | Ensures that quicksort indeed sorts the list.
+prop_quick_sorted :: [Int] -> Bool
+prop_quick_sorted xs = isSorted $ quicksort xs
+
+-- | Ensures that quicksort preserves the length of the list.
 prop_quick_length :: [Int] -> Bool
 prop_quick_length xs = (length xs) == length (quicksort xs)
 
@@ -18,4 +28,17 @@ prop_quick_elem :: [Int] -> Bool
 prop_quick_elem []     = True
 prop_quick_elem (x:xs) = elem x (quicksort (x:xs))
 
+-- | Ensures that mergesort indeed sorts the list.
+prop_merge_sorted :: [Int] -> Bool
+prop_merge_sorted xs = isSorted $ mergesort xs
 
+-- | Ensures that mergesort preserves the length of the list.
+prop_merge_length :: [Int] -> Bool
+prop_merge_length xs = length xs == (length $ mergesort xs)
+
+-- | Checks if a given list is sorted
+isSorted :: Ord a => [a] -> Bool
+isSorted []     = True
+isSorted (x:[]) = True
+isSorted (x:xs) | x <= head xs = isSorted xs
+                | otherwise    = False
