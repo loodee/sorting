d569b2e1047c01c9ce59685290a73c153d196369
Thomas Li <guslithd@student.gu.se>
2019-04-15 08:58:45 +0200

    fixed broken properties

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,7 +6,6 @@ main =
     do
      quickCheckWithResult stdArgs prop_quick_length >>= crash
      quickCheckWithResult stdArgs prop_quick_elem >>= crash
-     quickCheckWithResult stdArgs prop_quick_head >>= crash
 
 crash :: Result -> IO()
 crash Success {} = return ()
@@ -16,7 +15,7 @@ prop_quick_length :: [Int] -> Bool
 prop_quick_length xs = (length xs) == length (quicksort xs)
 
 prop_quick_elem :: [Int] -> Bool
+prop_quick_elem []     = True
 prop_quick_elem (x:xs) = elem x (quicksort (x:xs))
 
-prop_quick_head :: [Int] -> Bool
-prop_quick_head xs = 0 == head( quicksort (0:xs) )
+
